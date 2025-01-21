59022348ada65cad87959f9efc6727dbfbe11ae6
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2025-01-21 22:03:58 +0100

    Update Verify.js

--- src/Pages/Auth/Verify/Verify.js
+++ src/Pages/Auth/Verify/Verify.js
@@ -19,7 +19,7 @@ const Verify = () => {
     await dispatch(RESET());
     setTimeout(() => {
       navigate("/dashboard", { replace: true });
-      window.location.reload(); // Force a refresh of the next page
+      window.location.reload(); // Force a refresh of the page
     }, 3000);
   };
 
