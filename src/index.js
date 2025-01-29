2a3db95ed89005a1dab135a5570a2b091a9899a0
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2025-01-29 21:07:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want performance in your app, pass a function
+// s a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
