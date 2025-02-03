f4b22ae6804a25d787b0bcfa9d6062d46f664153
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2025-02-03 20:11:20 +0100

    Update authSlice.js

--- src/redux/features/auth/authSlice.js
+++ src/redux/features/auth/authSlice.js
@@ -319,7 +319,7 @@ const authSlice = createSlice({
       state.isSuccess = false;
       state.isLoading = false;
       state.message = "";
-      state.isLoggedIn = false; // Ensure this is set to false on RESET
+      state.isLoggedIn = false; //  this set to false on RESET
       state.user = null;
     },
     CALC_VERIFIED_USER(state, action) {
